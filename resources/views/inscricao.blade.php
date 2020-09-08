51e2b45b34905bb5ce0b2e21cb59e897ba7d8fc0
Milton Paiva <miltonpaiva@opovodigital.com>
2020-09-08 19:20:09 -0300

    refactor: adição de meta tag

--- resources/views/inscricao.blade.php
+++ resources/views/inscricao.blade.php
@@ -5,6 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+<!-- PERMITIR CONTEUDO MISTO TEMPORAREAMENTE -->
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
     <title>SIAP - Inscrição do Projetos</title>
 <link rel="stylesheet" href="{{ asset('css/style.css') }}">
 </head>
