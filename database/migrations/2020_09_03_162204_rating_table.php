08cb168e45bfe58e2a33404175f039491b3b8366
Milton Paiva <miltonpaiva@opovodigital.com>
2020-10-01 19:39:53 -0300

    tabela de avaliação

---
+++ database/migrations/2020_09_03_162204_rating_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class RatingTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rating', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('evaluator');
+            $table->unsignedBigInteger('startup');
+            $table->float('note', 2, 1);
+            $table->foreign('evaluator')->references('id')->on('users');
+            $table->foreign('startup')->references('id')->on('startups');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('rating');
+    }
+}
