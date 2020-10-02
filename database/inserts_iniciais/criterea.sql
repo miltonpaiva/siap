695033057eef2bfc64683edead4057f8d30d127a
Milton Paiva <miltonpaiva@opovodigital.com>
2020-10-02 08:53:29 -0300

    adição do sql de criterios

---
+++ database/inserts_iniciais/criterea.sql
@@ -0,0 +1,21 @@
+INSERT INTO public.criterea
+("id", "name", "type")
+VALUES(
+1, 'Experiência do time na tecnologia do produto', 'ambos'
+),(
+2, 'Experiência do time no setor econômico de atuação do projeto.', 'ambos'
+),(
+3, 'Apresentar no time competências multidisciplinares e complementares', 'ambos'
+),(
+4, 'Participação de mulheres na composição do time', 'ambos'
+),(
+5, 'Alinhamento com os produtos prioritários do programa', 'ambos'
+),(
+6, 'Alinhamento com as tendências tecnológicas prioritárias do programa', 'ambos'
+),(
+7, 'Alinhamento com os setores econômicos prioritários do programa', 'ambos'
+),(
+8, 'Nível de desenvolvimento do produto', 'tração'
+),(
+9, 'Nível de vendas realizadas', 'tração'
+);
