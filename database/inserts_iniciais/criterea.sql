bb562ce27a5a9a1670b00ce0e3e31a647424e54e
Milton Paiva <miltonpaiva@opovodigital.com>
2020-11-11 20:35:45 -0300

    refactor: mudança nas querys

--- database/inserts_iniciais/criterea.sql
+++ database/inserts_iniciais/criterea.sql
@@ -22,97 +22,12 @@ VALUES(
 
 INSERT INTO public.criterea
 ("name", "type", stage)
-VALUES(
-'Link do vídeo', 'criação', 'ambos'
-),
-(
-'Análise do setor', 'criação', 'atratividade'
-),
-(
-'Análise de mercado', 'criação', 'atratividade'
-),
-(
-'Análise de concorrentes', 'criação', 'atratividade'
-),
-(
-'Inovação de valor', 'criação', 'atratividade'
-),
-(
-'Descrição do problema', 'criação', 'atratividade'
-),
-(
-'Descrição da solução', 'criação', 'atratividade'
-),
-(
-'Descrição da vantagem competitiva', 'criação', 'atratividade'
-);
-
-
-
-INSERT INTO public.criterea
-("name", "type", stage)
-VALUES(
-'Qual é o problema?', 'tração', 'atratividade'
-),(
-'Quais evidências sustentam que o problema existe?', 'tração', 'atratividade'
-),(
-'O problema é urgente, mal atendido, impraticável e/ou inevitável?', 'tração', 'atratividade'
-),(
-'O problema é visível e/ou crítico?', 'tração', 'atratividade'
-),(
-'Quem é o cliente-alvo?', 'tração', 'atratividade'
-),(
-'Quais são as necessidades e desejos deles?', 'tração', 'atratividade'
-),(
-'Qual é o tamanho do mercado?', 'tração', 'atratividade'
-),(
-'Quais ganhos os clientes experimentarão?', 'tração', 'atratividade'
-),(
-'Que dores os clientes experimentarão?', 'tração', 'atratividade'
-),(
-'Quem são os concorrentes e seus clientes?', 'tração', 'atratividade'
-),(
-'Quais são os seus recursos mínimos viáveis?', 'tração', 'atratividade'
-),(
-'Por que esses recursos são valiosos e raros?', 'tração', 'atratividade'
-),(
-'Qual é o seu protótipo?', 'tração', 'atratividade'
-),(
-'Quais são as estratégias e preços dos concorrentes?', 'tração', 'atratividade'
-),(
-'Qual é a nossa estratégia e preços?', 'tração', 'atratividade'
-),(
-'Quais são seus canais de distribuição?', 'tração', 'atratividade'
-),(
-'Quem são seus intermediários?', 'tração', 'atratividade'
-),(
-'Qual é o plano e o orçamento de publicidade?', 'tração', 'atratividade'
-),(
-'Qual é o plano de relações públicas?', 'tração', 'atratividade'
-),(
-'Qual é o plano de vendas pessoais?', 'tração', 'atratividade'
-),(
-'Qual é o plano e o orçamento de marketing direto?', 'tração', 'atratividade'
-),(
-'Qual é o plano e orçamento de promoção de vendas?', 'tração', 'atratividade'
-),(
-'Quais tipos de fluxos de receita são usados pelos concorrentes?', 'tração', 'atratividade'
-),(
-'Que tipos de fluxos de receita usará?', 'tração', 'atratividade'
-),(
-'Quais são suas receitas estimadas no primeiro e no segundo ano?', 'tração', 'atratividade'
-),(
-'Quais são seus custos estimados detalhados no primeiro e no segundo ano?', 'tração', 'atratividade'
-),(
-'Qual é o seu processo de venda?', 'tração', 'atratividade'
-),(
-'Quais são suas necessidades de financiamento?', 'tração', 'atratividade'
-),(
-'Quem são suas fontes candidatas de financiamento?', 'tração', 'atratividade'
-),(
-'Quem são os fundadores?', 'tração', 'atratividade'
-),(
-'Quem são os mentores?', 'tração', 'atratividade'
-),(
-'Quem são os principais parceiros?', 'tração', 'atratividade'
-);
+VALUES
+('Modelo de receita','tração', 'atratividade')
+,('Escala','tração', 'atratividade')
+,('Problema e visão', 'ambos', 'atratividade')
+,('Problema e clusters regional ', 'ambos', 'atratividade')
+,('Proposta de valor', 'ambos', 'atratividade')
+,('Produto', 'ambos', 'atratividade')
+,('Mercado', 'ambos', 'atratividade')
+,('Você considera que o projeto avaliado realmente está apto para participar de um programa de empreendedorismo inovador?', 'ambos', 'atratividade');
---
+++ database/inserts_iniciais/questions_attractive.sql
@@ -0,0 +1,48 @@
+INSERT INTO public.questions_attractive ("id","name","type") VALUES 
+(10,'Link do vídeo','criação')
+,(11,'Análise do setor','criação')
+,(12,'Análise de mercado','criação')
+,(13,'Análise de concorrentes','criação')
+,(14,'Inovação de valor','criação')
+,(15,'Descrição do problema','criação')
+,(16,'Descrição da solução','criação')
+,(17,'Descrição da vantagem competitiva','criação')
+,(18,'Qual é o problema?','tração')
+,(19,'Quais evidências sustentam que o problema existe?','tração')
+;
+INSERT INTO public.questions_attractive ("id","name","type") VALUES 
+(20,'O problema é urgente, mal atendido, impraticável e/ou inevitável?','tração')
+,(21,'O problema é visível e/ou crítico?','tração')
+,(22,'Quem é o cliente-alvo?','tração')
+,(23,'Quais são as necessidades e desejos deles?','tração')
+,(24,'Qual é o tamanho do mercado?','tração')
+,(25,'Quais ganhos os clientes experimentarão?','tração')
+,(26,'Que dores os clientes experimentarão?','tração')
+,(27,'Quem são os concorrentes e seus clientes?','tração')
+,(28,'Quais são os seus recursos mínimos viáveis?','tração')
+,(29,'Por que esses recursos são valiosos e raros?','tração')
+;
+INSERT INTO public.questions_attractive ("id","name","type") VALUES 
+(30,'Qual é o seu protótipo?','tração')
+,(31,'Quais são as estratégias e preços dos concorrentes?','tração')
+,(32,'Qual é a nossa estratégia e preços?','tração')
+,(33,'Quais são seus canais de distribuição?','tração')
+,(34,'Quem são seus intermediários?','tração')
+,(35,'Qual é o plano e o orçamento de publicidade?','tração')
+,(36,'Qual é o plano de relações públicas?','tração')
+,(37,'Qual é o plano de vendas pessoais?','tração')
+,(38,'Qual é o plano e o orçamento de marketing direto?','tração')
+,(39,'Qual é o plano e orçamento de promoção de vendas?','tração')
+;
+INSERT INTO public.questions_attractive ("id","name","type") VALUES 
+(40,'Quais tipos de fluxos de receita são usados pelos concorrentes?','tração')
+,(41,'Que tipos de fluxos de receita usará?','tração')
+,(42,'Quais são suas receitas estimadas no primeiro e no segundo ano?','tração')
+,(43,'Quais são seus custos estimados detalhados no primeiro e no segundo ano?','tração')
+,(44,'Qual é o seu processo de venda?','tração')
+,(45,'Quais são suas necessidades de financiamento?','tração')
+,(46,'Quem são suas fontes candidatas de financiamento?','tração')
+,(47,'Quem são os fundadores?','tração')
+,(48,'Quem são os mentores?','tração')
+,(49,'Quem são os principais parceiros?','tração')
+;
