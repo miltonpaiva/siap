ac007736174ec64f5a02336a5ac5e3ebbcea7270
Milton Paiva <miltonpaiva@opovodigital.com>
2020-11-11 21:20:55 -0300

    feat: migration de avaliação

--- database/migrations/2020_09_03_162204_rating_attractive_table.php
+++ database/migrations/2020_09_03_162204_rating_attractive_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CommentsTable extends Migration
+class RatingAttractiveTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,15 @@ class CommentsTable extends Migration
      */
     public function up()
     {
-        Schema::create('comments', function (Blueprint $table) {
+        Schema::create('rating_attractive', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('evaluator');
             $table->unsignedBigInteger('startup');
-            $table->longText('comment')->nullable();
+            $table->unsignedBigInteger('criterea');
+            $table->int('note');
             $table->foreign('evaluator')->references('id')->on('users');
             $table->foreign('startup')->references('id')->on('startups');
-            $table->string('stage');
+            $table->foreign('criterea')->references('id')->on('criterea');
         });
     }
 
@@ -31,6 +32,6 @@ class CommentsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('comments');
+        Schema::dropIfExists('rating_attractive');
     }
 }
